c31310f367b8c7533050642b6cc23ab51065ce2c
AndreiKoenich <andrei.koenich@gmail.com>
2023-07-25 23:41:11 -0300

    Novas Coisas

--- src/components/Cadastro/index.js
+++ src/components/Cadastro/index.js
@@ -1,18 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
 
 export default function Cadastro({ route }) {
   const navigation = useNavigation();
   const [escolaridade, setEscolaridade] = useState('');
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [estado, setEstado] = useState('');
+  const [cidade, setCidade] = useState('');
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false);
 
-  // Verifica se há escolaridade selecionada da tela EscolaridadeScreen
-  React.useEffect(() => {
+  useEffect(() => {
     if (route.params?.escolaridade) {
       setEscolaridade(route.params.escolaridade);
     }
   }, [route.params]);
+
+  useEffect(() => {
+    if (nome && email && escolaridade && estado && cidade) {
+      setIsButtonEnabled(true);
+    } else {
+      setIsButtonEnabled(false);
+    }
+  }, [nome, email, escolaridade, estado, cidade]);
+
+  const handleCriarPerfilPress = () => {
+    console.log('Perfil criado com os seguintes dados:');
+    console.log('Nome:', nome);
+    console.log('E-mail:', email);
+    console.log('Escolaridade:', escolaridade);
+    console.log('Estado:', estado);
+    console.log('Cidade:', cidade);
+  };
 
   return (
     <View style={styles.container}>
@@ -21,23 +41,60 @@ export default function Cadastro({ route }) {
       </View>
 
       <Text style={styles.sectionText}>Quem é você?</Text>
-      <TextInput style={styles.input} placeholder="Nome" />
-      <TextInput style={styles.input} placeholder="E-mail" />
+      <TextInput
+        style={styles.input}
+        placeholder="Nome"
+        value={nome}
+        onChangeText={(text) => setNome(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="E-mail"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
 
       {/* Mostra a escolaridade selecionada ou o campo de seleção */}
-      <TouchableOpacity style={styles.input} onPress={() => navigation.navigate('EscolaridadeScreen')}>
-        <Text style={styles.placeholderText}>{escolaridade || 'Escolaridade'}</Text>
+      <TouchableOpacity style={[styles.input, escolaridade ? styles.selectedEscolaridade : null]} onPress={() => navigation.navigate('EscolaridadeScreen')}>
+        <Text style={[styles.placeholderText, escolaridade ? styles.selectedText : null]}>
+          {escolaridade || 'Escolaridade'}
+        </Text>
       </TouchableOpacity>
 
-      <TextInput style={styles.input} placeholder="Estado" />
-      <TextInput style={styles.input} placeholder="Cidade" />
+      <TextInput
+        style={styles.input}
+        placeholder="Estado"
+        value={estado}
+        onChangeText={(text) => setEstado(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Cidade"
+        value={cidade}
+        onChangeText={(text) => setCidade(text)}
+      />
+
+      <TouchableOpacity
+        style={[
+          styles.input,
+          styles.criarPerfilButton,
+          isButtonEnabled ? styles.buttonEnabled : styles.buttonDisabled,
+        ]}
+        onPress={isButtonEnabled ? handleCriarPerfilPress : undefined}
+        disabled={!isButtonEnabled}
+      >
+        <Text style={[styles.placeholderText, isButtonEnabled ? styles.buttonEnabledText : styles.buttonDisabledText]}>
+          Criar Perfil
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,    backgroundColor: '#ffffff',
+    flex: 1,
+    backgroundColor: '#ffffff',
     paddingHorizontal: 20,
     paddingTop: 40,
   },
@@ -68,5 +125,26 @@ const styles = StyleSheet.create({
   placeholderText: {
     color: '#888',
     fontSize: 16,
+  },
+  selectedEscolaridade: {
+    borderColor: 'black', // Cor da borda quando a escolaridade é selecionada
+  },
+  selectedText: {
+    color: 'black', // Cor do texto quando a escolaridade é selecionada
+  },
+  criarPerfilButton: {
+    alignItems: 'center',
+  },
+  buttonEnabled: {
+    backgroundColor: '#1E3799',
+  },
+  buttonDisabled: {
+    backgroundColor: '#ccc',
+  },
+  buttonEnabledText: {
+    color: 'black', // Cor do texto do botão quando está habilitado
+  },
+  buttonDisabledText: {
+    color: '#888', // Cor do texto do botão quando está desabilitado
   },
 });
