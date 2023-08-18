6bb226b38536f409339bddf79cc3d9e9125b6087
Andrei <andrei.koenich@gmail.com>
2023-08-18 18:21:41 -0300

    Update AlgoritmoCorrespondencia.js

--- src/components/AlgoritmoCorrespondencia.js
+++ src/components/AlgoritmoCorrespondencia.js
@@ -3,7 +3,7 @@
       descricao:
         'Desenvolvedor Web é responsável por criar e manter aplicações web, utilizando tecnologias como HTML, CSS, JavaScript e frameworks como React ou Angular. Além disso, trabalha com bancos de dados, serviços web e integração com APIs. É fundamental ter conhecimentos em programação e design para criar interfaces amigáveis e responsivas.',
       linguagens: ['JavaScript', 'HTML', 'CSS'],
-      frameworks: ['React JS  (JavaScript Library)', 'Angular (JavaScript Framework)', 'Node JS (JavaScript Framework)'],
+      frameworks: ['React.js', 'Angular.js', 'Vue.js', 'Express.js', 'Ruby on Rails'],
       tecnologias: ['Aprendizado de Máquina', 'Inteligência Artificial', 'Internet das Coisas'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -18,7 +18,7 @@
       descricao:
         'Desenvolvedor Mobile desenvolve aplicativos para dispositivos móveis, como smartphones e tablets. Utiliza linguagens como JavaScript, Kotlin ou Swift, e frameworks como React Native ou Android SDK. O profissional deve ter conhecimento em desenvolvimento de interfaces e experiência do usuário para criar aplicativos atraentes e funcionais.',
       linguagens: ['JavaScript', 'Kotlin', 'Swift', 'Java'],
-      frameworks: ['React Native  (JavaScript Library)', 'Android SDK', 'Node JS (JavaScript Framework)'],
+      frameworks: ['React Native', 'Ionic', 'Xamarin', 'Flutter', 'Figma', 'SwiftUI'],
       tecnologias: ['Aprendizado de Máquina', 'Inteligência Artificial', 'Computação em Nuvem'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -33,7 +33,7 @@
       descricao:
         'Engenheiro de Dados é responsável por projetar e manter a infraestrutura de dados da empresa. Utiliza tecnologias como Hadoop, Spark, bancos de dados NoSQL e SQL. O profissional deve ter habilidades em análise e modelagem de dados, além de conhecimentos em processamento e análise de grandes volumes de dados.',
       linguagens: ['Python', 'R', 'Java'],
-      frameworks: ['Django (Python Framework)','Flask (Python Framework)','Node JS (JavaScript Framework)'],
+      frameworks: ['Hadoop','Spark','Kafka','Flink','Airflow'],
       tecnologias: ['Aprendizado de Máquina', 'Big Data', 'Computação em Nuvem'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -48,7 +48,7 @@
       descricao:
         'Analista de Segurança da Informação é responsável por proteger as informações da empresa contra ameaças e ataques cibernéticos. Implementa políticas de segurança, realiza análises de vulnerabilidade e monitora a rede em busca de atividades suspeitas. O profissional deve ter conhecimento em segurança de redes, criptografia e proteção de dados.',
       linguagens: ['Python','Go', 'Ruby', 'C', 'C++' ],
-      frameworks: ['Django (Python Framework)', 'Ruby on Rails (Ruby Framework)', 'Spring Boot (JavaScript Framework)', 'Angular (JavaScript Framework)'],
+      frameworks: ['Metasploit','Wireshark','Nessus','Snort'],
       tecnologias: ['Aprendizado de Máquina', 'Big Data', 'Computação em Nuvem', 'Inteligência Artificial', 'Computação em Nuvem', 'Blockchain'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -63,7 +63,7 @@
       descricao:
         'Arquiteto de Software é responsável por projetar a arquitetura de sistemas de software, definindo sua estrutura, componentes e interações. Trabalha com linguagens de programação, frameworks e padrões de projeto para garantir a escalabilidade, confiabilidade e desempenho do sistema. O profissional deve ter habilidades em design de software e arquitetura de soluções.',
       linguagens: ['C#', 'C++', 'Python', 'Java'],
-      frameworks: ['React JS (JavaScript Library)', 'Node JS (JavaScript Framework)', 'Spring Boot (JavaScript Framework)', 'Angular (JavaScript Framework)'],
+      frameworks: ['Spring','ASP.NET','Ruby on Rails', 'Angular.js', 'Django'],
       tecnologias: ['Computação em Nuvem', 'Aprendizado de Máquina', 'Edge Computing'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -74,26 +74,11 @@
       salario_medio: 5786,
       url: 'https://roadmap.sh/software-architect',
     },
-    'Analista de Business Intelligence': {
-      descricao:
-        'Analista de Business Intelligence (BI) é responsável por coletar, analisar e transformar dados brutos em informações estratégicas para a empresa. Utiliza ferramentas de BI e bancos de dados para criar relatórios, dashboards e análises que auxiliem na tomada de decisões. O profissional deve ter conhecimento em análise de dados e modelagem de negócios.',
-      linguagens: ['Python', 'R', 'JavaScript', 'Java'],
-      frameworks: ['React JS (JavaScript Library)','Angular (JavaScript Framework)'],
-      tecnologias: ['Computação em Nuvem', 'Aprendizado de Máquina', 'Big Data'],
-      experiencia_banco_dados: true,
-      experiencia_desenvolvimento_agil: true,
-      experiencia_redes: true,
-      experiencia_e_commerce: false,
-      experiencia_desenvolvimento_mobile: false,
-      experiencia_migracao_legados: false,
-      salario_medio: 4525,
-      url: 'https://roadmap.sh/roadmaps',
-    },
     'Desenvolvedor Front-end': {
       descricao:
         'Desenvolvedor Front-end é responsável por criar a interface e a experiência do usuário em aplicações web. Utiliza linguagens como HTML, CSS e JavaScript, além de frameworks como React ou Angular. O profissional deve ter habilidades em design responsivo e usabilidade para criar interfaces amigáveis e atraentes.',
       linguagens: ['JavaScript', 'HTML', 'CSS', 'TypeScript', 'Python'],
-      frameworks: ['React JS  (JavaScript Library)', 'Angular (JavaScript Framework)'],
+      frameworks: ['React.js', 'Angular.js', 'Vue.js', 'Svelte', 'Ember.js'],
       tecnologias: ['Inteligência Artificial', 'Realidade Estendida', 'No-code/Low-code'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -108,7 +93,7 @@
       descricao:
         'Analista de Testes de Software é responsável por garantir a qualidade e a estabilidade de aplicações antes de serem lançadas. Utiliza técnicas de testes, como testes funcionais, de regressão e de desempenho, para identificar e corrigir falhas no sistema. O profissional deve ter conhecimentos em automação de testes e metodologias ágeis.',
       linguagens: ['Java', 'Python', 'TypeScript', 'JavaScript', 'Ruby'],
-      frameworks: ['Node JS (JavaScript Framework)', 'Angular (JavaScript Framework)', 'React JS (JavaScript Library)', 'React Native (JavaScript Library)'],
+      frameworks: ['Selenium', 'JUnit', 'TestNG', 'Cucumber', 'Appium'],
       tecnologias: ['Inteligência Artificial', 'Computação em Nuvem', 'Aprendizado de Máquina'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -123,7 +108,7 @@
       descricao:
         'Desenvolvedor Full-stack é responsável por trabalhar tanto no Front-end quanto no Back-end de aplicações web. Utiliza linguagens como HTML, CSS, JavaScript, além de frameworks como React ou Angular no Front-end, e frameworks como Node.js ou Spring Boot no Back-end. O profissional deve ter habilidades em programação e conhecimento geral em desenvolvimento web.',
       linguagens: ['JavaScript', 'HTML', 'CSS', 'Python', 'Java'],
-      frameworks: ['React JS  (JavaScript Library)', 'Angular (JavaScript Framework)', 'Node JS (JavaScript Framework)', 'Express.js (JavaScript Framework)'],
+      frameworks: ['React.js', 'Angular.js', 'Vue.js', 'Django', 'Node.js'],
       tecnologias: ['Inteligência Artificial', 'Computação em Nuvem', 'Aprendizado de Máquina', 'Big Data'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -138,7 +123,7 @@
       descricao:
         'Engenheiro de Machine Learning é responsável por desenvolver algoritmos e modelos de aprendizado de máquina para resolver problemas e tomar decisões. Utiliza linguagens como Python, bibliotecas de Machine Learning e técnicas de análise de dados para criar soluções inteligentes.',
       linguagens: ['Python', 'R', 'Java', 'C++'],
-      frameworks: [],
+      frameworks: ['TensorFlow', 'PyTorch', 'Scikit-Learn', 'Keras', 'MXNet'],
       tecnologias: ['Aprendizado de Máquina', 'Inteligência Artificial'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -153,7 +138,7 @@
       descricao:
         'Desenvolvedor Back-end é responsável por trabalhar na parte do servidor e infraestrutura de aplicações web. Utiliza linguagens como Java, Python ou PHP, e frameworks como Node.js ou Spring Boot. O profissional deve ter habilidades em desenvolvimento de APIs e conhecimentos em bancos de dados.',
       linguagens: ['Java', 'Python', 'JavaScript', 'C#', 'PHP'],
-      frameworks: ['Spring Boot  (JavaScript Framework)', 'Django (Python Framework)', 'Express.js (JavaScript Framework)', 'Ruby on Rails (Ruby Framework)'],
+      frameworks: ['Spring', 'ASP.NET', 'Ruby on Rails', 'Django', 'Node.js'],
       tecnologias: ['Big Data', 'Aprendizado de Máquina', 'Inteligência Artificial'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -168,7 +153,7 @@
       descricao:
         'Analista de Dados é responsável por coletar, organizar e analisar dados para extrair informações valiosas para a empresa. Utiliza ferramentas de análise de dados, como SQL, Excel e Python. O profissional deve ter conhecimento em estatística, modelagem de dados e visualização de informações.',
       linguagens: ['R', 'Python', 'Java', 'JavaScript'],
-      frameworks: [],
+      frameworks: ['Spark','Tableau', 'Tidyverse', 'Pandas', 'NumPy'],
       tecnologias: ['Big Data', 'Aprendizado de Máquina', 'Inteligência Artificial', 'Internet das Coisas'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -179,12 +164,11 @@
       salario_medio: 9900,
       url: 'https://roadmap.sh/roadmaps',
     },
-
     'Analista de Sistemas': {
       descricao:
         'Analista de Sistemas é responsável por analisar e projetar soluções de software para atender às necessidades da empresa. Trabalha com linguagens de programação, bancos de dados e frameworks para desenvolver sistemas e aplicações. O profissional deve ter habilidades em análise de requisitos e conhecimentos em desenvolvimento de software.',
       linguagens: ['Python', 'Java', 'JavaScript', 'C#'],
-      frameworks: ['Spring Boot  (JavaScript Framework)', 'Django (Python Framework)', 'React JS (JavaScript Library)', 'Angular (JavaScript Framework)', 'Flask (Python Framework)'],
+      frameworks: ['Scaled Agile', 'Scrum', 'BPMN (Business Process Model and Notation', 'ERD (Entity-Relationship Diagram)', 'TOGAF (The Open Group Architecture Framework)', 'UML (Unified Modeling Language)'],
       tecnologias: ['Big Data', 'Aprendizado de Máquina', 'Inteligência Artificial', 'Computação em Nuvem'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -199,7 +183,7 @@
       descricao:
         'Cientista de Dados é responsável por realizar análise de dados complexos, utilizando técnicas avançadas de estatística e aprendizado de máquina para desenvolver modelos preditivos e descritivos. O profissional deve ter habilidades em programação, matemática e análise de dados.',
       linguagens: ['Python', 'R'],
-      frameworks: ['Flask (Python Framework)', 'Django (Python Framework)'],
+      frameworks: ['TensorFlow', 'Scikit-Learn', 'Keras', 'Pandas', 'Spark', 'PyTorch', 'NumPy'],
       tecnologias: ['Aprendizado de Máquina', 'Big Data', 'Inteligência Artificial', 'Computação em Nuvem'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -214,7 +198,7 @@
       descricao:
         'Especialista em Redes é responsável por projetar, implementar e manter a infraestrutura de redes da empresa. Lida com switches, roteadores, firewalls e outras tecnologias para garantir a estabilidade e segurança da rede. O profissional deve ter conhecimento em protocolos de redes, segurança e virtualização.',
       linguagens: ['Python', 'C++', 'Java', 'JavaScript', 'Go'],
-      frameworks: [],
+      frameworks: ['Cisco', 'Wireshark', 'GNS3', 'Nmap', 'Open vSwitch'],
       tecnologias: ['Computação em Nuvem', 'Internet das Coisas', 'Edge Computing', 'Blockchain'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: false,
@@ -229,7 +213,7 @@
       descricao:
         'Especialista em E-commerce é responsável por gerenciar e otimizar as operações de comércio eletrônico da empresa. Trabalha com plataformas de e-commerce, marketing digital, logística e análise de dados para impulsionar as vendas online. O profissional deve ter conhecimento em negócios online e estratégias de marketing.',
       linguagens: ['Javascript', 'Python', 'PHP', 'Java', 'Ruby'],
-      frameworks: ['Ruby on Rails (Ruby Framework)', 'Django (Python Framework)', 'React JS (JavaScript Library)', 'Angular (JavaScript Framework)', 'Node JS (JavaScript Framework)'],
+      frameworks: ['Magento', 'Shopify', 'WooCommerce', 'BigCommerce'],
       tecnologias: ['Computação em Nuvem', 'Big Data', 'Inteligência Artificial'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -240,26 +224,11 @@
       salario_medio: 5231,
       url: 'https://roadmap.sh/blockchain',
     },
-    'Analista de Suporte Técnico': {
-      descricao:
-        'Analista de Suporte Técnico é responsável por prestar suporte e atendimento aos usuários da empresa, solucionando problemas em hardware, software e redes. O profissional deve ter habilidades em atendimento ao cliente, diagnóstico de problemas e conhecimentos técnicos em TI.',
-      linguagens: ['JavaScript', 'Python'],
-      frameworks: ['React JS (JavaScript Library)', 'Angular (JavaScript Framework)', 'Node JS (JavaScript Framework)'],
-      tecnologias: ['Computação em Nuvem', 'Internet das Coisas'],
-      experiencia_banco_dados: true,
-      experiencia_desenvolvimento_agil: true,
-      experiencia_redes: false,
-      experiencia_e_commerce: false,
-      experiencia_desenvolvimento_mobile: false,
-      experiencia_migracao_legados: true,
-      salario_medio: 4747,
-      url: 'https://roadmap.sh/roadmaps',
-    },
     'Especialista em Segurança Cibernética': {
       descricao:
         'Especialista em Segurança Cibernética é responsável por proteger a empresa contra ameaças virtuais. Implementa soluções de segurança, realiza testes de invasão e monitora a rede constantemente. O profissional deve ter conhecimento em técnicas de hacking ético, criptografia e políticas de segurança.',
       linguagens: ['Python', 'JavaScript', 'C++'],
-      frameworks: ['Django (Python Framework)', 'Flask (Python Framework)', 'Spring Boot (JavaScript Framework)'],
+      frameworks: ['NIST Cybersecurity', 'MITRE ATT&CK', ],
       tecnologias: ['Aprendizado de Máquina', 'Inteligência Artificial'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -274,7 +243,7 @@
       descricao:
         'Desenvolvedor de Jogos é responsável por criar jogos para diversas plataformas, como PC, consoles e dispositivos móveis. Utiliza linguagens como C#, C++ ou Java, e engines como Unity ou Unreal Engine. O profissional deve ter habilidades em programação, design de jogos e trabalho em equipe.',
       linguagens: ['C#', 'C++', 'Python'],
-      frameworks: ['React Native (JavaScript Library)'],
+      frameworks: ['Phaser', 'Godot', 'Unity', 'Unreal Engine'],
       tecnologias: ['Realidade Estendida', 'Inteligência Artificial'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -289,7 +258,7 @@
       descricao:
         'Analista de Qualidade de Software é responsável por garantir que os produtos e sistemas desenvolvidos atendam aos requisitos de qualidade. Realiza testes de software, identifica e relata defeitos, além de propor melhorias nos processos de desenvolvimento. O profissional deve ter conhecimentos em metodologias de teste e ferramentas de qualidade.',
       linguagens: ['JavaScript', 'Python', 'Java', 'C#'],
-      frameworks: ['PyTest (Python Framework)', 'JUnit (Java Framework)'],
+      frameworks: ['Selenium', 'JUnit', 'TestNG', 'Cucumber', 'Appium'],
       tecnologias: ['Aprendizado de Máquina', 'Inteligência Artificial', 'Realidade Estendida'],
       experiencia_banco_dados: true,
       experiencia_desenvolvimento_agil: true,
@@ -300,26 +269,11 @@
       salario_medio: 3621,
       url: 'https://roadmap.sh/roadmaps',
     },
-    'Desenvolvedor de Realidade Virtual': {
-      descricao:
-        'Desenvolvedor de Realidade Virtual é responsável por criar experiências e aplicações em ambiente de realidade virtual. Utiliza linguagens como C# ou C++, e engines como Unity ou Unreal Engine. O profissional deve ter habilidades em programação, design de interação e conhecimentos em realidade virtual.',
-      linguagens: ['C#', 'C++', 'Python'],
-      frameworks: ['Unity  (C# Framework)', 'Unreal Engine (C++ Framework)'],
-      tecnologias: ['Realidade Estendida', 'Computação em Nuvem', 'Internet das Coisas'],
-      experiencia_banco_dados: false,
-      experiencia_desenvolvimento_agil: true,
-      experiencia_redes: true,
-      experiencia_e_commerce: false,
-      experiencia_desenvolvimento_mobile: true,
-      experiencia_migracao_legados: false,
-      salario_medio: 5252,
-      url: 'https://roadmap.sh/roadmaps',
-    },
     'Analista de Marketing Digital': {
       descricao:
         'Analista de Marketing Digital é responsável por planejar e executar estratégias de marketing online. Utiliza ferramentas de análise de dados, gerenciamento de campanhas e mídias sociais para atrair e converter clientes. O profissional deve ter conhecimento em marketing digital e habilidades em comunicação.',
       linguagens: ['JavaScript', 'Python', 'PHP'],
-      frameworks: ['Angular (JavaScript Framework)', 'React JS (JavaScript Library)', 'Node.js (JavaScript Framework)'],
+      frameworks: ['Google Analytics', 'Googles Ads', 'HubSpot', 'Moz'],
       tecnologias: ['Computação em Nuvem', 'Big Data', 'Aprendizado de Máquina'],
       experiencia_banco_dados: false,
       experiencia_desenvolvimento_agil: true,
@@ -329,7 +283,8 @@
       experiencia_migracao_legados: false,
       salario_medio: 4108,
       url: 'https://roadmap.sh/roadmaps',
-    },
+    }
+
   };
 
 export let encontrarVagaAdequada = function(dados) {
