e7c5a50d997a985325a842b32160d1ec96c10ec8
Andrei <andrei.koenich@gmail.com>
2023-08-18 18:50:50 -0300

    Update AlgoritmoCorrespondencia.js

--- src/components/AlgoritmoCorrespondencia.js
+++ src/components/AlgoritmoCorrespondencia.js
@@ -2,7 +2,7 @@
     'Desenvolvedor Web': {
       descricao:
         'Desenvolvedor Web é responsável por criar e manter aplicações web, utilizando tecnologias como HTML, CSS, JavaScript e frameworks como React ou Angular. Além disso, trabalha com bancos de dados, serviços web e integração com APIs. É fundamental ter conhecimentos em programação e design para criar interfaces amigáveis e responsivas.',
-      linguagens: ['JavaScript', 'HTML', 'CSS'],
+      linguagens: ['JavaScript', 'HTML', 'CSS', 'Python', 'Java', 'PHP', 'Ruby','TypeScript', 'C#'],
       frameworks: ['React.js', 'Angular.js', 'Vue.js', 'Express.js', 'Ruby on Rails'],
       tecnologias: ['Aprendizado de Máquina', 'Inteligência Artificial', 'Internet das Coisas'],
       experiencia_banco_dados: true,
@@ -17,7 +17,7 @@
     'Desenvolvedor Mobile': {
       descricao:
         'Desenvolvedor Mobile desenvolve aplicativos para dispositivos móveis, como smartphones e tablets. Utiliza linguagens como JavaScript, Kotlin ou Swift, e frameworks como React Native ou Android SDK. O profissional deve ter conhecimento em desenvolvimento de interfaces e experiência do usuário para criar aplicativos atraentes e funcionais.',
-      linguagens: ['JavaScript', 'Kotlin', 'Swift', 'Java'],
+      linguagens: ['JavaScript', 'Kotlin', 'Swift', 'Java', 'Dart', 'C#'],
       frameworks: ['React Native', 'Ionic', 'Xamarin', 'Flutter', 'Figma', 'SwiftUI'],
       tecnologias: ['Aprendizado de Máquina', 'Inteligência Artificial', 'Computação em Nuvem'],
       experiencia_banco_dados: true,
@@ -32,7 +32,7 @@
     'Engenheiro de Dados': {
       descricao:
         'Engenheiro de Dados é responsável por projetar e manter a infraestrutura de dados da empresa. Utiliza tecnologias como Hadoop, Spark, bancos de dados NoSQL e SQL. O profissional deve ter habilidades em análise e modelagem de dados, além de conhecimentos em processamento e análise de grandes volumes de dados.',
-      linguagens: ['Python', 'R', 'Java'],
+      linguagens: ['Python', 'R', 'Java', 'Go', 'Ruby', 'Scala', 'SQL'],
       frameworks: ['Hadoop','Spark','Kafka','Flink','Airflow'],
       tecnologias: ['Aprendizado de Máquina', 'Big Data', 'Computação em Nuvem'],
       experiencia_banco_dados: true,
@@ -47,7 +47,7 @@
     'Analista de Segurança da Informação': {
       descricao:
         'Analista de Segurança da Informação é responsável por proteger as informações da empresa contra ameaças e ataques cibernéticos. Implementa políticas de segurança, realiza análises de vulnerabilidade e monitora a rede em busca de atividades suspeitas. O profissional deve ter conhecimento em segurança de redes, criptografia e proteção de dados.',
-      linguagens: ['Python','Go', 'Ruby', 'C', 'C++' ],
+      linguagens: ['Python','Go', 'Ruby', 'C', 'C++', 'Java', 'Javascript', 'Bash', 'PowerShell' ],
       frameworks: ['Metasploit','Wireshark','Nessus','Snort'],
       tecnologias: ['Aprendizado de Máquina', 'Big Data', 'Computação em Nuvem', 'Inteligência Artificial', 'Computação em Nuvem', 'Blockchain'],
       experiencia_banco_dados: true,
@@ -62,7 +62,7 @@
     'Arquiteto de Software': {
       descricao:
         'Arquiteto de Software é responsável por projetar a arquitetura de sistemas de software, definindo sua estrutura, componentes e interações. Trabalha com linguagens de programação, frameworks e padrões de projeto para garantir a escalabilidade, confiabilidade e desempenho do sistema. O profissional deve ter habilidades em design de software e arquitetura de soluções.',
-      linguagens: ['C#', 'C++', 'Python', 'Java'],
+      linguagens: ['C#', 'C++', 'Python', 'Java', 'Javascript', 'Ruby', 'PHP'],
       frameworks: ['Spring','ASP.NET','Ruby on Rails', 'Angular.js', 'Django'],
       tecnologias: ['Computação em Nuvem', 'Aprendizado de Máquina', 'Edge Computing'],
       experiencia_banco_dados: true,
@@ -77,7 +77,7 @@
     'Desenvolvedor Front-end': {
       descricao:
         'Desenvolvedor Front-end é responsável por criar a interface e a experiência do usuário em aplicações web. Utiliza linguagens como HTML, CSS e JavaScript, além de frameworks como React ou Angular. O profissional deve ter habilidades em design responsivo e usabilidade para criar interfaces amigáveis e atraentes.',
-      linguagens: ['JavaScript', 'HTML', 'CSS', 'TypeScript', 'Python'],
+      linguagens: ['JavaScript', 'HTML', 'CSS', 'TypeScript', 'Python', 'Ruby', 'Java', 'C#', 'Swift'],
       frameworks: ['React.js', 'Angular.js', 'Vue.js', 'Svelte', 'Ember.js'],
       tecnologias: ['Inteligência Artificial', 'Realidade Estendida', 'No-code/Low-code'],
       experiencia_banco_dados: true,
@@ -92,7 +92,7 @@
     'Analista de Testes de Software': {
       descricao:
         'Analista de Testes de Software é responsável por garantir a qualidade e a estabilidade de aplicações antes de serem lançadas. Utiliza técnicas de testes, como testes funcionais, de regressão e de desempenho, para identificar e corrigir falhas no sistema. O profissional deve ter conhecimentos em automação de testes e metodologias ágeis.',
-      linguagens: ['Java', 'Python', 'TypeScript', 'JavaScript', 'Ruby'],
+      linguagens: ['Java', 'Python', 'C#', 'JavaScript', 'Ruby', 'Swift', 'Kotlin', 'Perl', 'PHP'],
       frameworks: ['Selenium', 'JUnit', 'TestNG', 'Cucumber', 'Appium'],
       tecnologias: ['Inteligência Artificial', 'Computação em Nuvem', 'Aprendizado de Máquina'],
       experiencia_banco_dados: true,
@@ -107,7 +107,7 @@
     'Desenvolvedor Full-stack': {
       descricao:
         'Desenvolvedor Full-stack é responsável por trabalhar tanto no Front-end quanto no Back-end de aplicações web. Utiliza linguagens como HTML, CSS, JavaScript, além de frameworks como React ou Angular no Front-end, e frameworks como Node.js ou Spring Boot no Back-end. O profissional deve ter habilidades em programação e conhecimento geral em desenvolvimento web.',
-      linguagens: ['JavaScript', 'HTML', 'CSS', 'Python', 'Java'],
+      linguagens: ['JavaScript', 'HTML', 'CSS', 'Python', 'Java', 'Ruby', 'PHP', 'C#', 'TypeScript', 'Kotlin', 'Swift', 'Go'],
       frameworks: ['React.js', 'Angular.js', 'Vue.js', 'Django', 'Node.js'],
       tecnologias: ['Inteligência Artificial', 'Computação em Nuvem', 'Aprendizado de Máquina', 'Big Data'],
       experiencia_banco_dados: true,
@@ -122,7 +122,7 @@
     'Engenheiro de Machine Learning': {
       descricao:
         'Engenheiro de Machine Learning é responsável por desenvolver algoritmos e modelos de aprendizado de máquina para resolver problemas e tomar decisões. Utiliza linguagens como Python, bibliotecas de Machine Learning e técnicas de análise de dados para criar soluções inteligentes.',
-      linguagens: ['Python', 'R', 'Java', 'C++'],
+      linguagens: ['Python', 'R', 'Java', 'C++', 'Julia', 'MatLab', 'Scala', 'Go', 'Ruby', 'Rust'],
       frameworks: ['TensorFlow', 'PyTorch', 'Scikit-Learn', 'Keras', 'MXNet'],
       tecnologias: ['Aprendizado de Máquina', 'Inteligência Artificial'],
       experiencia_banco_dados: true,
@@ -137,7 +137,7 @@
     'Desenvolvedor Back-end': {
       descricao:
         'Desenvolvedor Back-end é responsável por trabalhar na parte do servidor e infraestrutura de aplicações web. Utiliza linguagens como Java, Python ou PHP, e frameworks como Node.js ou Spring Boot. O profissional deve ter habilidades em desenvolvimento de APIs e conhecimentos em bancos de dados.',
-      linguagens: ['Java', 'Python', 'JavaScript', 'C#', 'PHP'],
+      linguagens: ['Java', 'Python', 'Ruby', 'C#', 'PHP', 'Go', 'Kotlin', 'Rust', 'Swift', 'JavaScript'],
       frameworks: ['Spring', 'ASP.NET', 'Ruby on Rails', 'Django', 'Node.js'],
       tecnologias: ['Big Data', 'Aprendizado de Máquina', 'Inteligência Artificial'],
       experiencia_banco_dados: true,
@@ -152,7 +152,7 @@
     'Analista de Dados': {
       descricao:
         'Analista de Dados é responsável por coletar, organizar e analisar dados para extrair informações valiosas para a empresa. Utiliza ferramentas de análise de dados, como SQL, Excel e Python. O profissional deve ter conhecimento em estatística, modelagem de dados e visualização de informações.',
-      linguagens: ['R', 'Python', 'Java', 'JavaScript'],
+      linguagens: ['R', 'Python', 'Java', 'SQL', 'Scala', 'Julia', 'SAS', 'MatLab', 'Ruby', 'Perl'],
       frameworks: ['Spark','Tableau', 'Tidyverse', 'Pandas', 'NumPy'],
       tecnologias: ['Big Data', 'Aprendizado de Máquina', 'Inteligência Artificial', 'Internet das Coisas'],
       experiencia_banco_dados: true,
@@ -167,7 +167,7 @@
     'Analista de Sistemas': {
       descricao:
         'Analista de Sistemas é responsável por analisar e projetar soluções de software para atender às necessidades da empresa. Trabalha com linguagens de programação, bancos de dados e frameworks para desenvolver sistemas e aplicações. O profissional deve ter habilidades em análise de requisitos e conhecimentos em desenvolvimento de software.',
-      linguagens: ['Python', 'Java', 'JavaScript', 'C#'],
+      linguagens: ['Python', 'Java', 'JavaScript', 'C#', 'Ruby', 'PHP', 'Kotlin', 'Go', 'Typescript'],
       frameworks: ['Scaled Agile', 'Scrum', 'BPMN (Business Process Model and Notation', 'ERD (Entity-Relationship Diagram)', 'TOGAF (The Open Group Architecture Framework)', 'UML (Unified Modeling Language)'],
       tecnologias: ['Big Data', 'Aprendizado de Máquina', 'Inteligência Artificial', 'Computação em Nuvem'],
       experiencia_banco_dados: true,
@@ -182,7 +182,7 @@
     'Cientista de Dados': {
       descricao:
         'Cientista de Dados é responsável por realizar análise de dados complexos, utilizando técnicas avançadas de estatística e aprendizado de máquina para desenvolver modelos preditivos e descritivos. O profissional deve ter habilidades em programação, matemática e análise de dados.',
-      linguagens: ['Python', 'R'],
+      linguagens: ['Python', 'R', 'SQL', 'Java', 'Scala', 'Julia', 'MatLab', 'SAS', 'C++', 'Ruby'],
       frameworks: ['TensorFlow', 'Scikit-Learn', 'Keras', 'Pandas', 'Spark', 'PyTorch', 'NumPy'],
       tecnologias: ['Aprendizado de Máquina', 'Big Data', 'Inteligência Artificial', 'Computação em Nuvem'],
       experiencia_banco_dados: true,
@@ -197,7 +197,7 @@
     'Especialista em Redes': {
       descricao:
         'Especialista em Redes é responsável por projetar, implementar e manter a infraestrutura de redes da empresa. Lida com switches, roteadores, firewalls e outras tecnologias para garantir a estabilidade e segurança da rede. O profissional deve ter conhecimento em protocolos de redes, segurança e virtualização.',
-      linguagens: ['Python', 'C++', 'Java', 'JavaScript', 'Go'],
+      linguagens: ['Python', 'C++', 'Java', 'JavaScript', 'Go', 'Perl', 'PowerShell', 'C'],
       frameworks: ['Cisco', 'Wireshark', 'GNS3', 'Nmap', 'Open vSwitch'],
       tecnologias: ['Computação em Nuvem', 'Internet das Coisas', 'Edge Computing', 'Blockchain'],
       experiencia_banco_dados: true,
@@ -212,7 +212,7 @@
     'Especialista em E-commerce': {
       descricao:
         'Especialista em E-commerce é responsável por gerenciar e otimizar as operações de comércio eletrônico da empresa. Trabalha com plataformas de e-commerce, marketing digital, logística e análise de dados para impulsionar as vendas online. O profissional deve ter conhecimento em negócios online e estratégias de marketing.',
-      linguagens: ['Javascript', 'Python', 'PHP', 'Java', 'Ruby'],
+      linguagens: ['Javascript', 'Python', 'PHP', 'Java', 'Ruby', 'HTML', 'CSS', 'C#'],
       frameworks: ['Magento', 'Shopify', 'WooCommerce', 'BigCommerce'],
       tecnologias: ['Computação em Nuvem', 'Big Data', 'Inteligência Artificial'],
       experiencia_banco_dados: true,
@@ -227,7 +227,7 @@
     'Especialista em Segurança Cibernética': {
       descricao:
         'Especialista em Segurança Cibernética é responsável por proteger a empresa contra ameaças virtuais. Implementa soluções de segurança, realiza testes de invasão e monitora a rede constantemente. O profissional deve ter conhecimento em técnicas de hacking ético, criptografia e políticas de segurança.',
-      linguagens: ['Python', 'JavaScript', 'C++'],
+      linguagens: ['Python', 'JavaScript', 'C++', 'Java', 'PowerShell', 'Ruby', 'PHP', 'SQL', 'Bash', 'Go', 'C'],
       frameworks: ['NIST Cybersecurity', 'MITRE ATT&CK', ],
       tecnologias: ['Aprendizado de Máquina', 'Inteligência Artificial'],
       experiencia_banco_dados: true,
@@ -242,7 +242,7 @@
     'Desenvolvedor de Jogos': {
       descricao:
         'Desenvolvedor de Jogos é responsável por criar jogos para diversas plataformas, como PC, consoles e dispositivos móveis. Utiliza linguagens como C#, C++ ou Java, e engines como Unity ou Unreal Engine. O profissional deve ter habilidades em programação, design de jogos e trabalho em equipe.',
-      linguagens: ['C#', 'C++', 'Python'],
+      linguagens: ['C#', 'C++', 'Python','Java','Lua','JavaScript','Swift','Kotlin'],
       frameworks: ['Phaser', 'Godot', 'Unity', 'Unreal Engine'],
       tecnologias: ['Realidade Estendida', 'Inteligência Artificial'],
       experiencia_banco_dados: true,
@@ -257,7 +257,7 @@
     'Analista de Qualidade de Software': {
       descricao:
         'Analista de Qualidade de Software é responsável por garantir que os produtos e sistemas desenvolvidos atendam aos requisitos de qualidade. Realiza testes de software, identifica e relata defeitos, além de propor melhorias nos processos de desenvolvimento. O profissional deve ter conhecimentos em metodologias de teste e ferramentas de qualidade.',
-      linguagens: ['JavaScript', 'Python', 'Java', 'C#'],
+      linguagens: ['Java', 'Python',],
       frameworks: ['Selenium', 'JUnit', 'TestNG', 'Cucumber', 'Appium'],
       tecnologias: ['Aprendizado de Máquina', 'Inteligência Artificial', 'Realidade Estendida'],
       experiencia_banco_dados: true,
